19441f28b9e6d76ce85e63b2543a556ad1d7ee7f
Benjamin E Deckey <benjaminedeckey@Benjamins-MBP.localdomain>
2022-03-17 01:29:19 -0700

    adding diff route

--- src/App.js
+++ src/App.js
@@ -13,15 +13,7 @@ function App() {
         <Routes>
           <Route
             path="/"
-            element={<Home />}
-          />
-          <Route
-          path="/login"
-          element={<Login/>}
-          />
-          <Route
-          path="/select"
-          element={<Selector/>}
+            element={<Selector />}
           />
         </Routes>
       </Router>
