57ccd6e72a12c2d0d04aff6441e96ec7b88f4c99
Benjamin E Deckey <benjaminedeckey@Benjamins-MBP.localdomain>
2022-03-23 18:57:07 -0700

    changing lottie

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Selector from "./pages/Selector";
-import Leaderboard from "./pages/Leaderboard";
 import Splash from "./components/Splash";
-import animationData from "../src/assets/changingCar2.json";
+import animationData from "../src/assets/changingCar.json";
 import Login from "./pages/Login";
 
 function App() {
