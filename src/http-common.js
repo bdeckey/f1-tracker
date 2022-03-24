ca1eaff9ebdc2c7bc7cd2eaa9593123652428791
Benjamin E Deckey <benjaminedeckey@Benjamins-MBP.localdomain>
2022-03-23 17:40:16 -0700

    adding new code

--- src/http-common.js
+++ src/http-common.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:5000/api/v1",
-    // baseURL: "https://propertywatchers.herokuapp.com/api/v1/property",
+    // baseURL: "http://localhost:5000/api/v1",
+    baseURL: "https://fortune1-backend.herokuapp.com/api/v1",
     headers: {
         "Content-type": "application/json",
     }
